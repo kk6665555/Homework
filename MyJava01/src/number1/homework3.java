9c1e91407dcbe9360ee1f3732013ac7a4e26caf2
master <master@DESKTOP-4RO4EHN>
2017-03-15 23:54:35 +0800

    homework

---
+++ MyJava01/src/number1/homework3.java
@@ -0,0 +1,13 @@
+package number1;
+
+public class homework3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][][] a = new int[11][30][6];
+		for (int i = 0;i<a.length;i++){
+			
+		}
+	}
+
+}
