376e63a6faddc34b4e5dbca229a91f6026b78f3a
master <master@DESKTOP-4RO4EHN>
2017-04-19 22:05:53 +0800

    0419

---
+++ MyJava01/src/number1/Javaclock.java
@@ -0,0 +1,101 @@
+package number1;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+* @author sidwang
+*/
+public class Javaclock {
+
+public static void main(String[] args){
+JFrame ClockFrame = new JFrame("Java 指针時鐘");
+final ClockPanel clockPanel = new ClockPanel();
+
+ClockFrame.add(clockPanel);
+Timer uptime = new Timer(1000, new ActionListener(){
+@Override
+public void actionPerformed(ActionEvent ae) {
+clockPanel.upTimer();
+}
+});
+
+ClockFrame.setBounds(400,300,400,400);
+ClockFrame.setResizable(false);
+ClockFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ClockFrame.setVisible(true);
+uptime.start();
+}
+}
+
+class ClockPanel extends JPanel{
+Calendar calTime = new GregorianCalendar();
+String number[] = {"12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
+int sx,sy,mx,my,sx1,sy1;
+double hx,hy;
+int minute;
+int second;
+int hour;
+int OX = 200;
+int OY = 190;
+int i;
+
+public ClockPanel(){
+setBackground(Color.BLACK);
+setLayout(null);
+minute = calTime.get(Calendar.MINUTE); 
+second = calTime.get(Calendar.DATE);
+hour = calTime.get(Calendar.HOUR);
+}
+
+public void upTimer(){
+calTime.setTimeInMillis( System.currentTimeMillis() ); 
+repaint(); 
+}
+
+@Override
+public void paintComponent(Graphics grap){
+super.paintComponent(grap);
+minute = calTime.get(Calendar.MINUTE);
+second = calTime.get(Calendar.SECOND);
+hour = calTime.get(Calendar.HOUR);
+grap.setColor(Color.WHITE);
+grap.drawLine(OX, OY, (int)( OX+100*Math.sin(second*Math.PI/30) ), (int)( OY-100*Math.cos(second*Math.PI/30) ) );
+grap.drawLine(OX, OY, (int)(OX+20*Math.sin((second-30)*Math.PI/30)), (int)(OY-20*Math.cos((second-30)*Math.PI/30)));
+grap.setColor(Color.YELLOW);
+grap.drawLine(OX, OY, (int)(OX+80*Math.sin(minute*Math.PI/30)), (int)(OY-80*Math.cos(minute*Math.PI/30)) );
+grap.setColor(Color.RED);
+grap.drawLine(OX, OY, (int)(OX+55*Math.sin( (hour+minute/60.0) * Math.PI/6) ), (int)(OY-55*Math.cos( (hour+minute/60.0) * Math.PI/6) ));
+grap.setColor(Color.BLUE);
+paintNumber(grap);
+//grap.drawString("Name: Clock for Java", 20, 20);
+//grap.drawString("Author: Sid Wang"+"\n", 20, 35);
+//grap.drawString("Time: 2013-10-28", 20, 50);
+}
+
+public void paintNumber(Graphics gra){
+gra.setColor(Color.BLUE);
+for(i=0; i<60; i++){
+gra.drawLine((int)(OX-115*Math.cos(i*Math.PI/30)),
+(int)(OY+115*Math.sin(i*Math.PI/30)),
+(int)(OX-120*Math.cos(i*Math.PI/30)),
+(int)(OY+120*Math.sin(i*Math.PI/30)) );
+}
+
+for(i=0; i<12; i++){
+gra.setColor(Color.RED);
+gra.drawLine( (int)(OX-110*Math.cos(i*Math.PI/6)),
+(int)(OY+110*Math.sin(i*Math.PI/6)),
+(int)(OX-120*Math.cos(i*Math.PI/6)),
+(int)(OY+120*Math.sin(i*Math.PI/6)));
+gra.setColor(Color.lightGray); 
+gra.drawString(number[i], (int)(OX+135*Math.sin(i*Math.PI/6))-5,(int)(OY-135*Math.cos(i*Math.PI/6))+5 );
+}
+}
+}
--- MyJava01/src/number1/homework8.java
+++ MyJava01/src/number1/homework8.java
@@ -38,29 +38,26 @@ public class homework8  extends JFrame{
 
   class MyPool extends JPanel {
 	  private Timer a;
-	  private Time b;
-	  private Time1 c;
+	  private Time  b;
 	  int x=150,y=150;
 	 
 	  MyPool(){
 		  setBackground(Color.BLACK);
 		  a=new Timer();
-		  b=new Time(0,0,0);
-		  c=new Time1();
-		  a.schedule(b, 0,1000);
-		  a.schedule(c, 0, 100);
+		  b=new Time();
+		  a.schedule(b, 0,100);
+
 	  }
 	  
 		@Override
 		protected void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			Graphics2D g2d = (Graphics2D)g;	
-			g2d.setColor(Color.WHITE);
-			g2d.drawLine(x,y,(int) (x-100*Math.cos(-c.ss1*Math.PI/300)),(int)((y+100*Math.sin(-c.ss1*Math.PI/300))));
-			g2d.setColor(Color.BLUE);
-			g2d.drawLine(x, y,(int)(x+80*Math.sin(b.mm*Math.PI/30)), (int)(y-80*Math.cos(b.mm*Math.PI/30)) );
-			g2d.setColor(Color.CYAN);
-			g2d.drawLine(x,y,(int)(x+55*Math.sin( (b.yy+b.mm/60.0) * Math.PI/6) ), (int)(x-55*Math.cos( (b.yy+b.mm/60.0) * Math.PI/6) ));
+			super.paintComponent(g);			
+			g.setColor(Color.WHITE);
+			g.drawLine(x,y,(int) (x+100*Math.sin(b.ss*Math.PI/300)),(int)((y-100*Math.cos(b.ss*Math.PI/300))));
+			g.setColor(Color.BLUE);
+			g.drawLine(x, y,(int)(x+80*Math.sin(b.mm*Math.PI/30)), (int)(y-80*Math.cos(b.mm*Math.PI/30)) );
+			g.setColor(Color.CYAN);
+			g.drawLine(x,y,(int)(x+55*Math.sin( (b.yy+b.mm/60.0) * Math.PI/6) ), (int)(x-55*Math.cos( (b.yy+b.mm/60.0) * Math.PI/6) ));
 
 		}
 		// 畫1-12 和 每秒的刻度
@@ -90,37 +87,21 @@ public class homework8  extends JFrame{
 //現在的時間
   class Time extends TimerTask{
 	  private  int yy,mm,ss;
-	  Time(int yy,int mm, int ss){
-		  this.yy=yy;this.mm=mm;this.ss=ss;
-	  }
-	@Override
-	public void run() {
+	  Time(){
 		Calendar now = Calendar.getInstance();
 		yy=now.get(Calendar.HOUR_OF_DAY);
 		mm=now.get(Calendar.MINUTE);
-		ss=now.get(Calendar.SECOND);
-		repaint();
-	}
- }
-  
-  //讓秒針的個區隔變大
-  class Time1 extends TimerTask{
-	  private Time aa;
-	  private int ss1;
-	  Time1(){
-		 aa= new Time(0,0,0);
-		 ss1=(aa.ss-1)*10;
-		  this.ss1=ss1;
+		ss=((now.get(Calendar.SECOND)*10));
 	  }
 	@Override
-	public void run() {		
-		ss1++;	
+	public void run() {
+		ss++;
 		repaint();
 	}
-	  
-  }
+ }	  
+ }
 
-}
+
  
  
 
